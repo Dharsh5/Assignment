face5f50a8bfb3ee2babf1fef65c7bf50216eb65
Dharshana Srikanth <serapiscopio@gmail.com>
2016-01-14 20:06:18 +0530

    Update HappyPongal.c

--- HappyPongal.c
+++ HappyPongal.c
@@ -3,11 +3,11 @@
 int main()
 {
   int i;
-	char x[]="HAPPY PONGAL";
-	for(i=0;i<12;i++)
-	{
-    printf("%c",x[i]);
+  char x[]="HAPPY PONGAL";
+  for(i=0;i<12;i++)
+  {
+  	printf("%c",x[i]);
   	sleep(1);
-	}
-	return 0;
- }  
+  }
+  return 0;
+}  
