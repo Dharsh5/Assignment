059f35939c78782c27eb1e58416e227ac9eac1c1
Dharshana Srikanth <serapiscopio@gmail.com>
2016-03-05 13:24:17 +0530

    Create var.c

---
+++ var.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+int main()
+{
+  int count_let=0,i,n;
+  char str[20];
+  printf("enter string:");
+  scanf("%s",str);
+  for(i=0;str[i]!='\0';i++)
+  {
+    if(isalpha(str[i]))
+    {
+      count_let++;
+    }
+  }
+  printf("no of letters are : %d",count_let);
+  return 0;
+}
